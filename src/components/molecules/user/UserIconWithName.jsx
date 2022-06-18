9668fedde088efbdb85f77d0ee48bcdf30d9c1e9
guo133 <104018898+guo133@users.noreply.github.com>
2022-06-18 23:01:38 +0900

    state管理complete!

--- src/components/molecules/user/UserIconWithName.jsx
+++ src/components/molecules/user/UserIconWithName.jsx
@@ -1,14 +1,26 @@
-import styled from "styled-components";
+import React, { memo, useContext } from "react";
+import { useRecoilValue } from "recoil";
 
-export const UserIconWithName = (props) => {
+import styled from "styled-components";
+import { UserContext } from "../../../providers/UserProvider";
+import { userState } from "../../../store/userState";
+
+export const UserIconWithName = memo((props) => {
+  console.log("userIconwith");
+
   const { image, name } = props;
+  //const { userInfo } = useContext(UserContext);
+  const userInfo = useRecoilValue(userState);
+  const isAdmin = userInfo ? userInfo.isAdmin : false;
+
   return (
     <SContainer>
       <SImg height={160} width={160} src={image} alt={name} />
       <SName>{name}</SName>
+      {isAdmin && <SEdit>編集</SEdit>}
     </SContainer>
   );
-};
+});
 
 const SContainer = styled.div`
   text-align: center;
@@ -22,4 +34,10 @@ const SName = styled.p`
   font-weight: bold;
   margin: 0;
   color: #40514e;
+`;
+
+const SEdit = styled.span`
+  text-decoration: underline;
+  color: #aaa;
+  cursor: pointer;
 `;
--- src/components/pages/Users.jsx
+++ src/components/pages/Users.jsx
@@ -1,6 +1,13 @@
 import styled from "styled-components";
+import { useLocation } from "react-router-dom";
+import { useRecoilState } from "recoil";
+
 import { SearchInput } from "../molecules/SearchInput";
 import { UserCard } from "../organisms/user/UserCard";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
+//import { useContext } from "react";
+//import { UserContext } from "../../providers/UserProvider";
+import { userState } from "../../store/userState";
 
 const users = [...Array(10).keys()].map((val) => {
   return {
@@ -17,13 +24,23 @@ const users = [...Array(10).keys()].map((val) => {
 });
 
 export const Users = () => {
+  //const { userInfo, setUserInfo } = useContext(UserContext);
+
+  const [userInfo, setUserInfo] = useRecoilState(userState);
+
+  const onClickSwitch = () => {
+    setUserInfo({ isAdmin: !userInfo.isAdmin });
+  };
+
   return (
     <SContainer>
       <h2>ユーザ一覧</h2>
       <SearchInput />
+      <br />
+      <SecondaryButton onClick={onClickSwitch}>切り替え</SecondaryButton>
       <SUserArea>
-        {users.map((user) => (
-          <UserCard key={user.id} user={user} />
+        {users.map((obj) => (
+          <UserCard key={obj.id} user={obj} />
         ))}
       </SUserArea>
     </SContainer>
